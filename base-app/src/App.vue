1ad1dc6d9ddc1c14223d2a1313484644b42a405d
zhangHongEn <zhang_h_n@163.com>
2023-04-19 20:42:28 +0800

    mf demo

--- base-app/src/App.vue
+++ base-app/src/App.vue
@@ -9,11 +9,23 @@ import { loadMicroApp } from 'qiankun'
 export default {
   name: 'App',
   mounted() {
+    // 2种方法任选其一
+    // 1. 可以设置entry来加载子应用的app.js
     loadMicroApp({
       name: 'micro-app',
-      entry: '//localhost:9006',
-      container: this.$refs['container']
+      entry: {
+        scripts: ['//localhost:9006/app.js'],
+        html: "<div id=app></div>"
+      },
+      container: this.$refs['container'],
     })
+
+    // 2. 子应用已经对html-webpack-plugin做过去除mf入口的处理, 可以解析子应用的html加载子应用
+    // loadMicroApp({
+    //   name: 'micro-app',
+    //   entry: "//localhost:9006",
+    //   container: this.$refs['container'],
+    // })
   },
   methods: {
     handleClick() {
